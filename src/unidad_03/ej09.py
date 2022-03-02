c0d326e19785e2c080e809dc5ffada98710cf3a6
MCH <matias.david.chavez@gmail.com>
2022-03-02 20:40:35 -0300

    correcciones sobre el ej 9 y el readme de la unidad 3

--- src/unidad_03/ej09.py
+++ src/unidad_03/ej09.py
@@ -1,3 +1,5 @@
+import os
+
 """
 A partir del ejerció 7 cree un programa con 4 funciones:
 alta() para dar de alta la nueva compra
@@ -6,16 +8,6 @@ consulta() para consultar por todas las compras realizadas hasta el momento
 modificar() para modificar una compra realizada
 Pregunta: Considera que es más fácil guardar la información en
 listas o en diccionarios
-"""
-
-import os
-
-"""
-A partir del ejerció 6 cree un programa con 4 funciones:
-alta() para dar de alta la nueva compra
-baja() para dar de baja una compra
-consulta() para consultar por todas las compras realizadas hasta el momento
-modificar() para modificar una compra realizada
 """
 
 compra = dict()
